8f0a0f31036337d22f27b3211f4abc0fb8efc496
Daniil <daniiloreskin515@gmail.com>
2025-06-21 20:09:05 +0300

    Инциализировал тело игры, файл обрабатывает ввод команд от пользователя и выводит необходимую информацию на экран, таких как поле игры, информация о флажках, название игры

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "src/game/game.h"
+#include "src/plr_input/plr_input.h"
+#include "src/render/render.h"
+#include "src/output/output.h"
+
+static void new_game(GameInfo *game) {
+    init_game(game);
+    render_game(game);
+}
+
+int main() {
+    int playing = 1;
+
+    GameInfo game;
+    new_game(&game);
+
+    while (playing) {
+        int x, y;
+	int plr_action = get_plr_input(&x, &y);
+        
+        write_to_file(OUTPUT_LOG, "\n> Player input: ");
+        
+	switch(plr_action) {
+	    case ACTION_HELP:
+		write_to_file(OUTPUT_LOG, "Help");
+		// Помощь!!!
+		break;
+	    case ACTION_REVEAL:
+		write_to_file(OUTPUT_LOG, "Reveal (%d %d)", x, y);
+		reveal_cell(&game.board, x, y);
+		break;
+	    case ACTION_FLAG:
+		write_to_file(OUTPUT_LOG, "Flag (%d %d)", x, y);
+		toggle_flag(&game.board, x, y);
+		break;
+	    case ACTION_QUIT:
+		write_to_file(OUTPUT_LOG, "Quit");
+		playing = 0;
+		break;
+	    default:
+		write_to_file(OUTPUT_LOG, "Invalid");
+	        break;
+	}
+	
+	if (playing) {
+            update_game(&game);
+            render_game(&game);
+	}
+    }
+
+    return 0;
+}
