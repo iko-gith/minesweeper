2bd1df1df972753f60a679f3844b8499e5fb0309
Daniil <daniiloreskin515@gmail.com>
2025-06-21 20:28:26 +0300

    Инициализировал файл, включающий в себя функции, определяющие ввод пользователем комманд и очистку данного ввода

---
+++ src/plr_input/plr_input.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "plr_input.h"
+
+void clear_input() {
+    int c = getchar();
+    while ( c != '\n' && c != EOF )
+        c = getchar();
+}
+
+void print_help() {
+    printf("");
+}
+
+int get_plr_input(int *x, int *y) {
+    char cmd;
+    *x = -1;
+    *y = -1;
+
+    printf("\nPC Input: ");
+    
+    if (scanf(" %c", &cmd) != 1) {
+        clear_input();
+	return ACTION_INVALID;
+    }
+
+    cmd = toupper(cmd);
+    switch(cmd) {
+        case 'H':
+	    // Вывод комманд, позже    
+	    break;
+	case 'R':
+            if (scanf("%d %d", x, y) == 2)
+                return ACTION_REVEAL;
+	    break;
+	case 'F':
+	    if (scanf("%d %d", x, y) == 2)
+		return ACTION_FLAG;
+	    break;
+	case 'Q':
+	    return ACTION_QUIT;
+	default:
+	    break;
+    }
+
+    clear_input();
+    return ACTION_INVALID;
+}
---
+++ src/plr_input/plr_input.h
@@ -0,0 +1,17 @@
+#ifndef PLR_INPUT_H
+#define PLR_INPUT_H
+
+#include "../../constants.h"
+
+enum {
+    ACTION_INVALID = 0,
+    ACTION_HELP    = 1,
+    ACTION_REVEAL  = 2,
+    ACTION_FLAG    = 3,
+    ACTION_QUIT    = 4
+};
+
+void clear_input();
+int get_plr_input(int *x, int *y);
+
+#endif
