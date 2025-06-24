a7ed309db34e621c3f4c02922006180f87ea4639
Daniil <daniiloreskin515@gmail.com>
2025-06-24 21:49:38 +0300

    Добавленна функция update_bomb_count, позволяющая обновить количество бомб на поле

--- src/game/game.c
+++ src/game/game.c
@@ -3,15 +3,14 @@
 
 void init_game(GameInfo* game) {
     // Инициализация поля
-    init_board(&game->board);
+    if (game->bombs)
+	init_board(&game->board, game->bombs);
     
     // Инициализация данных игры
     if (!game->board.cells) {
-        game->state = GAME_NOT_INIT;
-	game->mines_left = 0;
+        game->state      = GAME_NOT_INIT;
     } else {
-        game->state = GAME_PROC;
-	game->mines_left = BOMBS_COUNT;
+        game->state      = GAME_PROC;
     }
 }
 
@@ -32,9 +31,13 @@ void update_game(GameInfo* game) {
         game->state = GAME_WON;
     } else {
 	write_to_file(OUTPUT_LOG, "In process");
-	game->mines_left = count_bombs_left(&game->board);
         game->state = GAME_PROC;
     }
 }
 
-
+void update_bomb_count(GameInfo *game, int bomb_count) {
+    if (!bomb_count || bomb_count <= 0)
+	bomb_count = 1;
+    
+    game->bombs = bomb_count;
+}
