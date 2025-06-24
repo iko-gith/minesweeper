e5e8cd14f2c0f97d95c2dd03ae895517ca7b66ed
Daniil <daniiloreskin515@gmail.com>
2025-06-24 19:04:53 +0300

    В связи появлением данных структуры Board (height, width), была необходимость поменять BOARD_SIZE на соответствующие данные в функциях

--- src/render/render.c
+++ src/render/render.c
@@ -25,17 +25,19 @@ void render_cell(const Cell *cell) {
 }
 
 void render_board(const Board *board) {
+    int height = board->height;
+    int width  = board->width;
     printf("\n   ");
-    for (int x = 0; x < BOARD_SIZE; x++)
-        printf("%2d%s", x, (x+1>=BOARD_SIZE) ? "\n" : "");
+    for (int x = 0; x < height; x++)
+        printf("%2d%s", x, (x+1>=height) ? "\n" : "");
 
     printf("   +");
-    for (int x = 0; x < BOARD_SIZE; x++)
-	printf("--%s", (x+1>=BOARD_SIZE) ? "+\n" : "");
+    for (int x = 0; x < height; x++)
+	printf("--%s", (x+1>=height) ? "+\n" : "");
 
-    for (int x = 0; x < BOARD_SIZE; x++) {
+    for (int x = 0; x < height; x++) {
 	printf("%2d |", x);
-        for (int y = 0; y < BOARD_SIZE; y++) {
+        for (int y = 0; y < width; y++) {
 	    render_cell(&board->cells[x][y]);
 	}
 
@@ -43,8 +45,8 @@ void render_board(const Board *board) {
     }
 
     printf("   +");
-    for (int x = 0; x < BOARD_SIZE; x++)
-        printf("--%s", (x+1>=BOARD_SIZE) ? "+\n" : "");
+    for (int x = 0; x < height; x++)
+        printf("--%s", (x+1>=height) ? "+\n" : "");
 }
 
 void render_header(const GameInfo *game) {
@@ -66,11 +68,14 @@ void render_header(const GameInfo *game) {
 	    printf("Not initialized\n");
 	    break;
     }
-    
 }
 
 void render_game(const GameInfo *game) {
     clear_screen();
-    render_header(game);
-    render_board(&game->board);
+    if (game->state == GAME_NOT_INIT) {
+	printf("Please setup settings for a board using (S height width)\n");
+    } else {
+	render_header(game);
+        render_board(&game->board);
+    }
 }
